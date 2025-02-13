3e2d3d1e14cca543d5d80b51d0b25e08dcd59db2
Aidan Levy <168674488+AidanLevy05@users.noreply.github.com>
2025-02-13 13:26:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,74 @@
 #include "Matrix.h"
-using namespace std;
+#include <cstdlib>
+#include <string>
 
-int main()
-{
+void div();
+void div(string);
+void showAllDimensions(Matrix&, Matrix&, Matrix&, char);
 
-  srand(time(0));
+int main() {
 
-  string file;
-  cout << "Please enter a file name: " << endl;
-  cin >> file;
+    srand(time(0));
+    string file = "matrix.txt";
 
-  Matrix matrix1(file);
-  vector<double> solutions;
+    // populate matrix.txt
+    const int ROWS = 10;
+    const int COLS = ROWS;
+    const int RAND = 1000;
+    ofstream outFile;
+    outFile.open(file);
 
-  try
-  {
-    cout << "Before RREF:" << endl
-         << endl;
-    matrix1.display();
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            outFile << rand() % RAND << " ";
+        }
+        outFile << endl;
+    }
+
+    outFile.close();
+
+
+    Matrix matrix(file);
+
+    div("Before RREF");
+    matrix.display();
+
+    try {
+        vector<double> sols = matrix.rref();
+        int numElements = sols.size();
+        div("After RREF");
+        matrix.display();
+        cout << "Solutions work: " << matrix.solve(sols, numElements) << endl;
+        matrix.parametricVectorForm();
+    } catch (const exception& e) {
+        cerr << e.what() << endl;
+    }
+
+    div();
+
+    return 0;
+}
+
+void div() {
     cout << endl;
+    for (int i = 0; i < 30; i++) cout << "-";
+    cout << endl << endl;
+}
 
-    solutions = matrix1.rref();
-
-    cout << "After RREF:" << endl
-         << endl;
-    matrix1.display();
+void div(string text) {
     cout << endl;
-  }
-  catch (const exception &e)
-  {
-    cout << e.what() << endl;
-  }
+    cout << text << "  ";
+    for (int i = 0; i < 30; i++) cout << "-";
+    cout << endl << endl;
+}
+
+void showAllDimensions(Matrix& m1, Matrix& m2, Matrix& m3, char character) {
+    cout << "Matrix 1: ";
+    m1.dimensions();
+    cout << "\t\t" << character << endl;
+    cout << "Matrix 2: ";
+    m2.dimensions();
+    cout << "\t\t=" << endl;
+    cout << "Matrix 3: ";
+    m3.dimensions();
 }
