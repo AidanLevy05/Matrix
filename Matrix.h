fbd86eaf12e9894a302254d24cb939809dbc5fc0
Aidan Levy <168674488+AidanLevy05@users.noreply.github.com>
2025-03-24 08:51:50 -0400

    Update Matrix.h
    Changed double** matrix to double matrix[][]

--- Matrix.h
+++ Matrix.h
@@ -1,5 +1,8 @@
 #ifndef MATRIX_H
-#define MATRIX_H 
+#define MATRIX_H
+
+#define MAX_COLS 100
+#define MAX_ROWS 100
 
 #include <iostream>
 #include <cmath>
@@ -10,60 +13,59 @@
 #include <algorithm>
 #include <numeric>
 #include <stdexcept>
-#include <iomanip>
 using namespace std;
 
-class Matrix {
+class Matrix
+{
 
 private:
-    double** matrix;
-    int rows;
-    int cols;
+  int rows;
+  int cols;
+  double matrix[MAX_ROWS][MAX_COLS];
 
-    bool isValid() const;
-    double greatestCD(vector<double>);
-    void swapRows(int, int);
-    void multiplyRow(int, double);
-    void replaceRow(int, int, double);
-    bool infiniteSolutions(int);
-    bool noSolutions(int);
-    bool canSimplifyRow(int);
+  bool isValid() const;
+  double greatestCD(vector<double>);
+  void swapRows(int, int);
+  void multiplyRow(int, double);
+  void replaceRow(int, int, double);
+  bool infiniteSolutions(int);
+  bool noSolutions(int);
+  bool canSimplifyRow(int);
 
 public:
-    // Constructors and destructor
-    Matrix();
-    Matrix(int, int);
-    Matrix(int, int, double);
-    Matrix(string);
-    Matrix(const Matrix&);
-    ~Matrix();
+  // Constructors and destructor
+  Matrix();
+  Matrix(int, int);
+  Matrix(int, int, double);
+  Matrix(string);
+  Matrix(const Matrix &);
+  ~Matrix();
 
-    // Basic functions
-    void setValue(int, int, double);
-    void setRandom(int);
-    double getValue(int, int);
-    void setDimensions(int, int);
-    int getRows();
-    int getCols();
-    void dimensions();
-    void display();
+  // Basic functions
+  void setValue(int, int, double);
+  void setRandom(int);
+  double getValue(int, int);
+  void setDimensions(int, int);
+  int getRows();
+  int getCols();
+  void dimensions();
+  void display();
 
-    // Solving
-    bool solve(vector<double>&, int);
-    void parametricForm();
-    void parametricForm(vector<double>);
-    void parametricVectorForm();
-    vector<double> rref();
-    bool isRREF();
-    bool noSolutions();
+  // Solving
+  bool solve(vector<double> &, int);
+  void parametricForm();
+  void parametricForm(vector<double>);
+  void parametricVectorForm();
+  vector<double> rref();
+  bool isRREF();
+  bool noSolutions();
 
-    // Overloaded operators
-    double* operator[](int);
-    Matrix& operator=(const Matrix&);
-    Matrix operator*(Matrix&);
-    Matrix operator+(Matrix&);
-    Matrix operator-(Matrix&);
-
+  // Overloaded operators
+  double *operator[](int);
+  Matrix &operator=(const Matrix &);
+  Matrix operator*(Matrix &);
+  Matrix operator+(Matrix &);
+  Matrix operator-(Matrix &);
 };
 
 #endif /* MATRIX_H */
